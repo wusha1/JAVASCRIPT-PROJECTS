45d03691472802bbafe529dd61c01e3518f79b7b
wusha1 <allcompass11@outlook.com>
2026-01-21 23:00:46 -0800

    Completed Basic JavaScript Project submission requirements

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,5 +1,15 @@
-// This variable stores the player's name as a string
-var playerName = "Victor", playerScore = 100, levelNumber = 1;
+// This alert displays a message when the page loads
+window.alert("Welcome to my Basic JavaScript Project!");
 
-// This line displays the player's name on the webpage
-document.write(playerName);
+// These variable store string values
+var Sent1 = "This is the beginning of the string";
+var Sent2 = "and this is the end of the string";
+
+// This line concatenates the two strings and writes them to the page
+document.write(Sent1 + Sent2);
+
+// This is an example of a simple expression
+var result = 5 + 10;
+
+// This writes the result of the exprssion to the page
+document.write("<br>Expression result:" + result);
