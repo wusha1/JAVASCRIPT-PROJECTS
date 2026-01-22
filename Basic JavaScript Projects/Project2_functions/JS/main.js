f464817f236c99199ea2c4bea42159bf94bb5569
wusha1 <allcompass11@outlook.com>
2026-01-22 00:11:16 -0800

    Added Project2_functions with JavaScript function

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function myFunction(){
+    // Assigning two variables
+    var part1="Hello";
+    var part2="World";
+
+    // Combining the two variables
+    var fullMessage= part1 + part2;
+
+    // Using getElemenTById to display the message
+    document.getElementById("output").innerHTML=fullMessage;
+}
