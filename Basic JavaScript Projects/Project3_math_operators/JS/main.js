ef5c887eb22b41255c1fb91d48a55aef98bd444e
wusha1 <allcompass11@outlook.com>
2026-01-22 15:57:07 -0800

    Added Project3_math_operators assignment

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,7 @@
+function addNumbers() {
+    // Perform an addition operation
+    var result = 5 + 10;
+
+    // Display the result im the paragraph element
+    document.getElementById("Math").innerHTML = result;
+}
