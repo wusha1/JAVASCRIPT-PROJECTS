c28357baaf76dc30b7650f287ac28f64fa62e016
wusha1 <allcompass11@outlook.com>
2026-01-21 22:45:59 -0800

    Added comments explaining JavaScript code

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,5 @@
-document.write(5 + 10);
+// This variable stores the player's name as a string
+var playerName = "Victor", playerScore = 100, levelNumber = 1;
+
+// This line displays the player's name on the webpage
+document.write(playerName);
