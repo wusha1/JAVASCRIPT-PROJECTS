67f09f0be62bbade0fb5aada6bd5cb9659b3da83
wusha1 <allcompass11@outlook.com>
2026-01-23 19:52:43 -0800

    Added Infinity and -Infinity assignment

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,8 +1,2 @@
-// Display NaN by attempting an invalid math operation
-document.write(0 / 0 + "<br>");
-
-// Use isNaN() to display true
-document.write(isNaN("Hello") + "<br>");
-
-// Use isNaN() to display false
-document.write(isNaN(10));
+document.getElementById("Infinity").innerHTML =
+  2E310 + "<br>" + -2E310;
