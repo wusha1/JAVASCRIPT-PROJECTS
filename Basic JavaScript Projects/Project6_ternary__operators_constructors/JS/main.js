0945371c7712e9072d383f75de2321075c94e2e4
wusha1 <allcompass11@outlook.com>
2026-01-23 21:43:59 -0800

    Completed keywords and constructors assignment

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,12 +1,4 @@
-// Ternary Operator Function
-function Ride_Function() {
-    var Height, Can_ride;
-    Height = document.getElementById("Height").value;
-    Can_ride = (Height < 52) ? "You are too short" : "You are tall enough";
-    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
-}
-
-// Constructor Function
+// Constructor function
 function Vehicle(Make, Model, Year, Color) {
     this.Vehicle_Make = Make;
     this.Vehicle_Model = Model;
@@ -14,13 +6,15 @@ function Vehicle(Make, Model, Year, Color) {
     this.Vehicle_Color = Color;
 }
 
-// New object using constructor
+// Objects created using the "new" keyword
 var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 
-// Function displaying constructor data
+// Function displaying object data
 function myFunction() {
     document.getElementById("Keywords_and_Constructors").innerHTML =
-        "Jack drives a " + Jack.Vehicle_Color + " " +
-        Jack.Vehicle_Model + " manufactured in " +
-        Jack.Vehicle_Year;
+        "Erik drives a " + Erik.Vehicle_Color + "-colored " +
+        Erik.Vehicle_Model + " manufactured in " +
+        Erik.Vehicle_Year;
 }
