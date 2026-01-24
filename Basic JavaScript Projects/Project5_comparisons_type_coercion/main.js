1ce550df1865e130f77c283c504606aa768d1684
wusha1 <allcompass11@outlook.com>
2026-01-23 21:05:49 -0800

    Added AND and OR operator examples

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/main.js
@@ -0,0 +1,7 @@
+// AND operator examples
+console.log(10 > 5 && 5 > 1);   // true
+console.log(10 > 20 && 5 > 1);  // false
+
+// OR operator examples
+console.log(10 > 20 || 5 > 1);  // true
+console.log(10 > 20 || 5 < 1);  // false
