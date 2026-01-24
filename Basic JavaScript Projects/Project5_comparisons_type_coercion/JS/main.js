25309287041c4f2891545d4a998ea3d99333c6bf
wusha1 <allcompass11@outlook.com>
2026-01-23 20:32:52 -0800

    Added triple equal signs comparison assignment

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,5 +1,11 @@
-// Using == to return true (type coercion occurs)
-console.log("5" == 5);
+// TRUE: same data type and same value
+console.log(10 === 10);
 
-// Using == to return false
-console.log(10 == 5);
+// FALSE: different data type and different value
+console.log("5" === 10);
+
+// FALSE: different data type but same value
+console.log("5" === 5);
+
+// FALSE: same data type but different value
+console.log(7 === 3);
