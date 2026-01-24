319eee70b9b7858e910f9ef333ab154de76a01d6
wusha1 <allcompass11@outlook.com>
2026-01-24 00:36:54 -0800

    Completed nested functions assignment

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,12 +1,7 @@
-// Constructor Function (VALID CODE)
-function Animal(Type, Age) {
-    this.Animal_Type = Type;
-    this.Animal_Age = Age;
+function outerFunction() {
+    document.getElementById("Nested_Function").innerHTML = innerFunction();
+
+    function innerFunction() {
+        return "This text came from a nested function!";
+    }
 }
-
-// Creating an object using the constructor
-var Dog = new Animal("Dog", 5);
-
-// Displaying constructor result
-document.getElementById("Result").innerHTML =
-    "The animal is a " + Dog.Animal_Type + " and is " + Dog.Animal_Age + " years old.";
