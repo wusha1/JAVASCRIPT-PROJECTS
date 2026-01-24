be344c3904cdd0bff5544892ba9ec8ab76b78f21
wusha1 <allcompass11@outlook.com>
2026-01-23 18:58:04 -0800

    Completed Project3_math_operators submission requirements

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,9 +1,38 @@
-function useMathObject() {
-    // Use a Math object method
-    var number = 4.7;
-    var roundedNumber = Math.round(number);
+// Addition
+function addNumbers() {
+    document.getElementById("Math").innerHTML = 5 + 5;
+}
 
-    // Display the result
-    document.getElementById("Math").innerHTML =
-        "Rounded value: " + roundedNumber;
+// Subtraction
+function subtractNumbers() {
+    document.getElementById("Math").innerHTML = 10 - 3;
+}
+
+// Multiplication
+function multiplyNumbers() {
+    document.getElementById("Math").innerHTML = 4 * 6;
+}
+
+// Modulus
+function modulusNumbers() {
+    document.getElementById("Math").innerHTML = 25 % 6;
+}
+
+// Increment
+function incrementNumber() {
+    var num = 10;
+    num++;
+    document.getElementById("Math").innerHTML = num;
+}
+
+// Decrement
+function decrementNumber() {
+    var num = 10;
+    num--;
+    document.getElementById("Math").innerHTML = num;
+}
+
+// Math.random
+function randomNumber() {
+    document.getElementById("Math").innerHTML = Math.random();
 }
