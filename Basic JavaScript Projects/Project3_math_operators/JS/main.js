8a7a8b04fff3b10e4a6153ac1b58721afe88a276
wusha1 <allcompass11@outlook.com>
2026-01-23 18:41:57 -0800

    Added modulus operator example

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,20 +1,8 @@
-function multipleOperators() {
-    // Addition
-    var addition = 10 + 5;
+function showRemainder() {
+    // Modulus operation (remainder)
+    var remainder = 25 % 6;
 
-    // Subtraction
-    var subtraction = 20 - 8;
-
-    // Multiplication
-    var multiplication = 4 * 6;
-
-    // Division
-    var division = 24 / 4;
-
-    // Display all results
+    // Display the result
     document.getElementById("Math").innerHTML =
-        "Addition result: " + addition +
-        "<br>Subtraction result: " + subtraction +
-        "<br>Multiplication result: " + multiplication +
-        "<br>Division result: " + division;
+        "The remainder is: " + remainder;
 }
