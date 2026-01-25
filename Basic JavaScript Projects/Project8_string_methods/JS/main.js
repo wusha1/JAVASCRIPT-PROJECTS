400d7a152bf28adc5999f2c6a7ed3cbd9f23be3f
wusha1 <allcompass11@outlook.com>
2026-01-25 00:01:06 -0800

    Submit Project 8 string methods assignment

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,21 +1,52 @@
-// toFixed() METHOD
-// Formats a number to a fixed number of decimal places
-function useToFixed() {
-    var num = 7.45678;
+// concat() METHOD
+// Joins multiple strings together
+function useConcat() {
+    var part1 = "Learning ";
+    var part2 = "JavaScript ";
+    var part3 = "is fun!";
 
-    // Format number to 2 decimal places
-    var result = num.toFixed(2);
+    var result = part1.concat(part2, part3);
 
-    document.getElementById("Fixed_Result").innerHTML =
-        "Number formatted with toFixed(): " + result;
+    document.getElementById("Concat_Result").innerHTML = result;
+}
+
+// slice() METHOD
+// Extracts a section of a string
+function useSlice() {
+    var text = "JavaScript string methods are useful";
+
+    var result = text.slice(0, 10);
+
+    document.getElementById("Slice_Result").innerHTML = result;
+}
+
+// toString() METHOD
+// Converts a number into a string
+function useToString() {
+    var number = 100;
+
+    var result = number.toString();
+
+    document.getElementById("ToString_Result").innerHTML =
+        "Number as string: " + result;
+}
+
+// toPrecision() METHOD
+// Formats a number to a specified length
+function usePrecision() {
+    var number = 123.456789;
+
+    var result = number.toPrecision(5);
+
+    document.getElementById("Precision_Result").innerHTML =
+        "Number with precision: " + result;
 }
 
 // valueOf() METHOD
-// Returns the primitive value of a Number object
+// Returns the value of a Number object
 function useValueOf() {
     var numObject = new Number(25);
 
-    // Get the primitive value
     var result = numObject.valueOf();
 
     document.getElementById("Value_Result").innerHTML =
