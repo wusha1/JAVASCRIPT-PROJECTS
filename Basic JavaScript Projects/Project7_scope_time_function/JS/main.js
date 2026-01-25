bbab4084b06099d6b608bbbb0830596bf8333d61
wusha1 <allcompass11@outlook.com>
2026-01-24 22:02:48 -0800

    Completed Project 7 else statement assignment

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,9 +1,15 @@
-function Check_Number() {
-    var Number = 10;
+function Check_Age() {
+    var Age = document.getElementById("User_Age").value;
+    var Message;
 
-    // Simple if statement
-    if (Number > 5) {
-        document.getElementById("If_Result").innerHTML =
-            "The number is greater than 5.";
+    // if / else statement
+    if (Age >= 18) {
+        Message = "You are old enough to continue.";
     }
+    else {
+        Message = "You are not old enough to continue.";
+    }
+
+    // Displays the result in the HTML element
+    document.getElementById("Age_Result").innerHTML = Message;
 }
