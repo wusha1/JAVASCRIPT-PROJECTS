c8b20fe0c6744c770bf5fcecbec8144810715ca4
wusha1 <allcompass11@outlook.com>
2026-01-24 22:14:03 -0800

    Submit Project 7 scope and time function assignment

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,15 +1,51 @@
-function Check_Age() {
-    var Age = document.getElementById("User_Age").value;
-    var Message;
+// Global Variable
+// This variable is available throughout the entire file
+var Global_Number = 10;
 
-    // if / else statement
-    if (Age >= 18) {
-        Message = "You are old enough to continue.";
+// FUNCTION WITH IF STATEMENT
+// Uses a local variable and an if statement
+function Check_Number() {
+
+    // LOCAL VARIABLE
+    var Local_Number = 5;
+
+    // If statement comparing global and local variables
+    if (Global_Number > Local_Number) {
+        document.getElementById("If_Result").innerHTML =
+            "The global number is greater than the local number.";
+    }
+
+    // Call function that contains an intentional error
+    Error_Function();
+}
+
+// FUNCTION WITH INTENTIONAL ERROR
+// Attempts to access a local variable that is out of scope
+// console.log is used to debug in Chrome DevTools
+function Error_Function() {
+
+    // This will cause an error because Local_Number
+    // is not defined in this scope
+    console.log(Local_Number);
+}
+
+// TIME FUNCTION
+// Uses Date().getHours() as outlined in previous slides
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+
+    // if / else if / else structure based on time
+    if (Time < 12) {
+        Reply = "Good morning!";
+    }
+    else if (Time < 18) {
+        Reply = "Good afternoon!";
     }
     else {
-        Message = "You are not old enough to continue.";
+        Reply = "Good evening!";
     }
 
-    // Displays the result in the HTML element
-    document.getElementById("Age_Result").innerHTML = Message;
+    // Displays time-based message on the webpage
+    document.getElementById("Time_Message").innerHTML = Reply;
 }
