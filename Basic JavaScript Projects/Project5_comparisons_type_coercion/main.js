afa6e5fdedff036e67c58e0e6878f6bcfe0377b4
wusha1 <allcompass11@outlook.com>
2026-01-24 20:07:03 -0800

    Completed and resubmitted Project 5 comparisons and type coercion requirements

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/main.js
@@ -1,7 +1,40 @@
-// AND operator examples
-console.log(10 > 5 && 5 > 1);   // true
-console.log(10 > 20 && 5 > 1);  // false
+// TYPEOF OPERATOR + document.write()
 
-// OR operator examples
-console.log(10 > 20 || 5 > 1);  // true
-console.log(10 > 20 || 5 < 1);  // false
+// Variable to check data type
+var myNumber = 42;
+
+// Display the data type of the variable
+document.write("Type of myNumber: " + typeof myNumber + "<br><br>");
+
+
+// STRING + NUMBER 
+
+// Combining a string and a number
+document.write("My age is " + 30 + "<br><br>");
+
+
+// COMPARISON OPERATORS
+
+// Equality operator ==
+document.write("10 == 10 : " + (10 == 10) + "<br>");
+
+// Strict equality operator ===
+document.write("10 === '10' : " + (10 === "10") + "<br>");
+
+// Greater than operator >
+document.write("15 > 10 : " + (15 > 10) + "<br>");
+
+// Less than operator <
+document.write("5 < 10 : " + (5 < 10) + "<br><br>");
+
+
+// LOGICAL OPERATORS
+
+// AND operator &&
+document.write("10 > 5 && 5 > 2 : " + (10 > 5 && 5 > 2) + "<br>");
+
+// OR operator ||
+document.write("10 < 5 || 5 > 2 : " + (10 < 5 || 5 > 2) + "<br>");
+
+// NOT operator !
+document.write("!(10 > 5) : " + !(10 > 5) + "<br>");
