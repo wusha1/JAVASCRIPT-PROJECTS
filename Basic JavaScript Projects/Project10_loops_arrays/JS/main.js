07eebcefb378a4b7b7b9137a3708c41c75626508
wusha1 <allcompass11@outlook.com>
2026-01-27 02:22:40 -0800

    Add array function using innerHTML

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,13 @@
+function array_Function() {
+    let instruments = [
+        "Guitar",
+        "Drums",
+        "Piano",
+        "Bass",
+        "Violin",
+        "Saxophone"
+    ];
+
+    document.getElementById("Array").innerHTML =
+        "One of my favorite instruments is the " + instruments[2] + ".";
+}
