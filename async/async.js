01148fb44373b4a7db73fcdfe00a3addec2f5b3b
wusha1 <allcompass11@outlook.com>
2026-01-26 22:06:10 -0800

    Completed defer script loading challenge

---
+++ async/async.js
@@ -0,0 +1,1 @@
+document.getElementById("p1").innerHTML = "JavaScript is working!";
---
+++ defer/defer.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "The defer attribute is working!";
+}
