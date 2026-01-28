43a029c8e7b3def2d6eb6bcd7e2ea4ea7c300efe
wusha1 <allcompass11@outlook.com>
2026-01-27 22:40:34 -0800

    Add const object assignment with updated properties

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,13 +1,18 @@
-function array_Function() {
-    let instruments = [
-        "Guitar",
-        "Drums",
-        "Piano",
-        "Bass",
-        "Violin",
-        "Saxophone"
-    ];
+function constant_function() {
+    const instrument = {
+        type: "Guitar",
+        brand: "Fender",
+        color: "Black"
+    };
 
-    document.getElementById("Array").innerHTML =
-        "One of my favorite instruments is the " + instruments[2] + ".";
+    // Changes a property value
+    instrument.color = "Red";
+
+    // Adds a new property with a value
+    instrument.condition = "Brand new";
+
+    // Displays a string using the changed and added properties
+    document.getElementById("Constant").innerHTML =
+        "The " + instrument.color + " " + instrument.brand + " " + instrument.type +
+        " is " + instrument.condition + ".";
 }
