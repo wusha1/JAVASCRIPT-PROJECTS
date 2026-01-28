089c4e36b3998d340ccf4669bb05bf9b37a5e5f1
wusha1 <allcompass11@outlook.com>
2026-01-27 22:42:55 -0800

    Add let keyword example

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,18 +1,5 @@
-function constant_function() {
-    const instrument = {
-        type: "Guitar",
-        brand: "Fender",
-        color: "Black"
-    };
+function let_function() {
+    let message = "The let keyword allows variables to be reassigned.";
 
-    // Changes a property value
-    instrument.color = "Red";
-
-    // Adds a new property with a value
-    instrument.condition = "Brand new";
-
-    // Displays a string using the changed and added properties
-    document.getElementById("Constant").innerHTML =
-        "The " + instrument.color + " " + instrument.brand + " " + instrument.type +
-        " is " + instrument.condition + ".";
+    document.getElementById("Constant").innerHTML = message;
 }
