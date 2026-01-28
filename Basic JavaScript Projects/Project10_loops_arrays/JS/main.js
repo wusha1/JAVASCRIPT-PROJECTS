28483a8a6a3d3b98531746ff545b96ad7d21e1ad
wusha1 <allcompass11@outlook.com>
2026-01-27 23:09:41 -0800

    Complete Project10 loops and arrays assignment

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,24 +1,50 @@
-function loop_Function() {
+// WHILE LOOP FUNCTION
+// function uses a while loop to count from 1 to 5
+function while_Function() {
+    let count = 1;
     let text = "";
-    let i;
 
-    // Break example
-    for (i = 0; i < 10; i++) {
-        if (i === 5) {
-            break;
-        }
-        text += "Break loop value: " + i + "<br>";
+    while (count <= 5) {
+        text += "While loop count: " + count + "<br>";
+        count++;
     }
 
-    text += "<br>";
+    document.getElementById("While_Loop").innerHTML = text;
+}
 
-    // Continue example
-    for (i = 0; i < 10; i++) {
-        if (i === 5) {
-            continue;
-        }
-        text += "Continue loop value: " + i + "<br>";
+// FOR LOOP FUNCTION
+// function uses a for loop to count from 0 to 4
+function for_Function() {
+    let text = "";
+
+    for (let i = 0; i < 5; i++) {
+        text += "For loop number: " + i + "<br>";
     }
 
-    document.getElementById("Loop_Result").innerHTML = text;
+    document.getElementById("For_Loop").innerHTML = text;
+}
+
+// ARRAY FUNCTION
+// function uses an array and displays one value from it
+function array_Function() {
+    let fruits = ["Apple", "Banana", "Orange", "Grapes"];
+
+    document.getElementById("Array").innerHTML =
+        "My favorite fruit is " + fruits[1] + ".";
+}
+
+// OBJECT FUNCTION
+// function creates an object using the let keyword
+function object_Function() {
+    let car = {
+        make: "Toyota",
+        model: "Tacoma",
+        year: 2022,
+        description: function () {
+            return this.year + " " + this.make + " " + this.model;
+        }
+    };
+
+    document.getElementById("Object").innerHTML =
+        "Vehicle: " + car.description();
 }
