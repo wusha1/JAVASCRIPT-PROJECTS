b37df5431b7846b13fff446dd67013ea4580d8f4
wusha1 <allcompass11@outlook.com>
2026-01-28 22:53:44 -0800

    Add getElementsByClassName assignment

--- Advanced JavaScript Projects/JavaScript/JavaScript_1.js
+++ Advanced JavaScript Projects/JavaScript/JavaScript_1.js
@@ -1,20 +1,4 @@
-function animal_Function() {
-    let animal;
-    let animal_choice = document.getElementById("Animal_Input").value;
-
-    switch (animal_choice) {
-        case "Dog":
-            animal = "Dogs are loyal and friendly.";
-            break;
-        case "Cat":
-            animal = "Cats are independent and curious.";
-            break;
-        case "Bird":
-            animal = "Birds are colorful and love to sing.";
-            break;
-        default:
-            animal = "Please enter Dog, Cat, or Bird.";
-    }
-
-    document.getElementById("Output").innerHTML = animal;
+function class_Function() {
+    let elements = document.getElementsByClassName("Click");
+    elements[1].innerHTML = "The text has been changed using getElementsByClassName!";
 }
