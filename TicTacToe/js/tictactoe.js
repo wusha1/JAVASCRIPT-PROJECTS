a8341f60023b8009c8457056fba497e34759abb1
wusha1 <allcompass11@outlook.com>
2026-01-30 00:52:37 -0800

    Add checkWinConditions for Tic Tac Toe

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -1,59 +1,85 @@
 // Keeps track of whose turn it is
 let activePlayer = "X";
 
+// Stores selected squares
+let selectedSquares = [];
+
 // Places X or O in the chosen square
 function placeXorO(squareNumber) {
-    // Prevent overwriting a square
     if (document.getElementById(squareNumber).innerHTML !== "") {
         return false;
     }
 
-    // Place the player's letter (course may later change to images)
     document.getElementById(squareNumber).innerHTML = activePlayer;
+    selectedSquares.push(squareNumber + activePlayer);
 
-    // This function plays placement sound.
     audio('./media/place.mp3');
 
-    // Switch active player
+    checkWinConditions();
+
     if (activePlayer === "X") {
         activePlayer = "O";
     } else {
         activePlayer = "X";
     }
 
-    // This condition checks to see if it is the computers turn.
     if (activePlayer === 'O') {
-        // This function disables clicking for computers turn.
         disableClick();
-        // This function waits 1 second before the computer places an image and enables click.
-        setTimeout(function () { computersTurn(); }, 1000);
+        setTimeout(function () { computersTurn(); enableClick(); }, 1000);
     }
 
-    // Returning true is needed for our computersTurn() function to work.
     return true;
 }
 
 // This function results in a random square being selected by the computer.
 function computersTurn() {
-    // This boolean is needed for our while loop.
     let success = false;
-    // This variable stores a random number 0-8.
     let pickASquare;
 
-    // This condition allows our while loop to keep trying if a square is selected already.
     while (!success) {
-        // A random number between 0 and 8 is selected.
         pickASquare = String(Math.floor(Math.random() * 9));
-
-        // If the random number evaluated returns true, the square hasn't been selected yet.
         if (placeXorO(pickASquare)) {
-            // This changes our boolean and ends the loop.
             success = true;
         }
     }
 }
 
-/* Helper functions (only keep these if your course file doesn't already have them) */
+// This function parses the selectedSquares array to search for win conditions.
+// drawWinLine() function is called to draw a line on the screen if the condition is met.
+function checkWinConditions() {
+
+    if (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100); }
+    else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304); }
+    else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508); }
+    else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558); }
+    else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 304, 558); }
+    else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558); }
+    else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90); }
+    else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520); }
+
+    else if (arrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 100, 558, 100); }
+    else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304); }
+    else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(50, 508, 558, 508); }
+    else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558); }
+    else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558); }
+    else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(508, 50, 508, 558); }
+    else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 508, 510, 90); }
+    else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520); }
+
+    else if (selectedSquares.length >= 9) {
+        audio('./media/tie.mp3');
+        setTimeout(function () { resetGame(); }, 500);
+    }
+}
+
+// This function checks if an array includes 3 strings.
+function arrayIncludes(squareA, squareB, squareC) {
+    const a = selectedSquares.includes(squareA);
+    const b = selectedSquares.includes(squareB);
+    const c = selectedSquares.includes(squareC);
+
+    if (a === true && b === true && c === true) { return true; }
+}
 
 function disableClick() {
     document.getElementById("body").style.pointerEvents = "none";
