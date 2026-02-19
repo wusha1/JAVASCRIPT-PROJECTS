8bbe1f315e245a8687394bb6c517e63015074453
wusha1 <allcompass11@outlook.com>
2026-02-19 14:56:25 -0800

    Add todo.js with localStorage todo list rendering

---
+++ todo_app/js/todo.js
@@ -0,0 +1,51 @@
+/* this function gets the task from input*/
+function get_todos() {
+    /*This creates an array of tasks that are inputed*/
+    var todos = new Array;
+    /*this pulls the task that was saved in the web browser memory*/
+    var todos_str = localStorage.getItem('todo');
+    /*If the input is not null then JSON.parse will
+    communicate with the web browser to make the task a JavaScript object.*/
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str);
+    }
+    return todos;
+}
+
+/*this function adds the inputed task to the get_todos function array*/
+function add() {
+    /*This takes the inputed task and creates a variable of it*/
+    var task = document.getElementById('task').value;
+
+    var todos = get_todos();
+    /*This adds a new task the end of the array*/
+    todos.push(task);
+    /* this converts the task input to a JSON string*/
+    localStorage.setItem('todo', JSON.stringify(todos));
+    document.getElementById("task").value = "";
+    show();
+
+    return false;
+}
+
+/*this function keeps the tasks permanently displayed on the screen*/
+function show() {
+    /* this sets the task that was retrieved as a variable*/
+    var todos = get_todos();
+
+    /*This sets up each task as an unordered list*/
+    var html = '<ul>';
+    /*This displays a task to the list in the order that it is inputed*/
+    for (var i = 0; i < todos.length; i++) {
+        /*This also displays the task as a list and creates the button with the "x"*/
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></li>';
+    };
+    html += '</ul>';
+    /*This displays the task as a list*/
+    document.getElementById('todos').innerHTML = html;
+}
+
+/*This displays the inputed task when the 'Add Item' button is clicked*/
+document.getElementById('add').addEventListener('click', add);
+/*this will keep the inputs displayed permantaly on the screen*/
+show();
