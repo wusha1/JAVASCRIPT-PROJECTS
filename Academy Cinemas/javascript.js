a94019521cee8646f0df7904d7e14ea9fcc47552
wusha1 <allcompass11@outlook.com>
2026-02-18 19:33:12 -0800

    Navbar shrink updated

--- Academy Cinemas/javascript.js
+++ Academy Cinemas/javascript.js
@@ -34,3 +34,35 @@ function displaySelectedMovieOptions() {
 function buyTickets() {
   displaySelectedMovieOptions();
 }
+
+//JQUERY
+
+//Shrinks header size when the document is scrolled down by 80 pixels
+$(document).on("scroll", function () {
+
+    //When the webpage is scrolled down from the top by 50px this
+    //if statement will trigger
+    if ($(document).scrollTop() > 50) {
+
+        //Once the 50px requirement has been met add the
+        //nav-shrink class selector to the same HTML element
+        //that has the nav class
+        $("nav").addClass("nav-shrink");
+
+        //Adjust the position of the mobile drop menu
+        //to accommodate the new height decrease
+        $("div.navbar-collapse").css("margin-top", "-6px");
+
+    } else {
+
+        //if the webpage has not been scrolled down or
+        //is back at the top the nav-shrink class selector
+        //is removed from the HTML element with the nav
+        //class selector
+        $("nav").removeClass("nav-shrink");
+
+        //The margin for the drop down menu is now
+        //returned to it's original amount
+        $("div.navbar-collapse").css("margin-top", "14px");
+    }
+});
